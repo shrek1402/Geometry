68cb1dfd476a78edfecbd7bdef9d71b274b4b8a4
Митрохин Антон Игоревич <shrek.2000@yandex.ru>
2019-02-28 23:03:55 +0700

    Входные данные

--- Geometry2/Geometry2.cpp
+++ Geometry2/Geometry2.cpp
@@ -1,21 +1,77 @@
-﻿// Geometry2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// Geometry2.cpp : Этот файл содержит функцию "main". Здесь начинается и
+// заканчивается выполнение программы.
 //
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    setlocale(LC_ALL, "Russian");
+    {
+        string sStart,
+            sName,
+            sCoordinate;
+
+        //Входные данные//
+        cin >> sStart;
+        {
+            size_t s = sStart.size(),
+                   i = 0,
+                   j = 0;
+
+            while (sStart.at(i) != '(' && i < sStart.size() - 1) {
+                i++;
+            }
+
+            if (i > sStart.size() - 1) {
+                cout << "ERROR 1" << endl;
+                exit(123);
+            }
+
+            sName = sStart.substr(0, i);
+
+            j = i - 1;
+
+            while (sStart.at(j) != ')' && j < sStart.size() - 1) {
+                j++;
+            }
+
+            if (sStart.at(sStart.size() - 1) != ')') {
+                cout << "ERROR 2" << endl;
+                exit(123);
+            }
+
+            sCoordinate = sStart.substr(i + 1, j - i - 1);
+            cout << "Со скобочками все ОК" << endl;
+        }
+
+		
+
+		vector<tCircle> vCircle;
+        vector<tTrinagle> vTrinagle;
+		vector<tPoligone> vPoligone;
+
+        if (sName == "trinagle") {
+            cout << "trinagle" << endl;
+        }
+
+        else if (sName == "circle") {
+            cout << "circle" << endl;
+        }
+
+
+        else if (sName == "poligone") {
+            cout << "poligone" << endl;
+        }
+
+        else {
+            cout << "ERROR" << endl;
+            exit(404);
+        }
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
